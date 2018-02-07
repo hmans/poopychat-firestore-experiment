109407877d23cf873d20cb4a83475759f2405f25
Hendrik Mans <hendrik@mans.de>
2018-02-07 16:16:22 +0100

    Let's do this!

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.container {
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <article v-for="(message, idx) in messages" :key="idx">
+      <h1>{{ message.message }}</h1>
+    </article>
+  </div>
+</template>
+
+<script>
+import { db } from '../main'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      messages: []
+    }
+  },
+  firestore () {
+    return {
+      messages: db.collection('messages').orderBy('createdAt')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
